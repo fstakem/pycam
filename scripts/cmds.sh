b86b45a6a5c42ea9e526c6cae385b70afef44703
Fredrick <fstakem@gmail.com>
2018-03-25 17:01:51 -0400

    Update cmds.sh
    Add more details on flashing

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -22,9 +22,9 @@ Ctrl-a x
 # Install
 pip install rshell
 
-# Run
+# Run: pyboard shows up on ttyACM0
 rshell --port /dev/ttyUSB0 --baud 115200 --buffer-size 128 --editor nano
-sudo ./pycam_repl_env/bin/rshell --port /dev/ttyUSB0 --baud 115200 --buffer-size 128 --editor vim
+sudo ~/envs/upy_env/bin/rshell --port /dev/ttyUSB0 --baud 115200 --buffer-size 128 --editor vim
 
 # Copy files
 cp ./pycam/arducam.py /pyboard/arducam.py
@@ -32,5 +32,7 @@ cp ./pycam/arducam_constants.py /pyboard/arducam_constants.py
 cp ./pycam/main.py /pyboard/main.py
 
 # Flash new firmware with esptool
+sudo ~/envs/upy_env/bin/esptool.py --port /dev/ttyUSB0 erase_flash
 sudo ~/envs/upy_env/bin/esptool.py --chip esp32 -p /dev/ttyUSB0 write_flash -z 0x1000 <firmware_path>
+
 
